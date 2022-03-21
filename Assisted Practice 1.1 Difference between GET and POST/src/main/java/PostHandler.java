9e08fe2f228f6e5c3bdf731d353836919948f349
kapildavey <99812570+kapildavey@users.noreply.github.com>
2022-03-21 19:43:14 +0530

    Add files via upload

---
+++ Assisted Practice 1.1 Difference between GET and POST/src/main/java/PostHandler.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+* Servlet implementation class PostHandler
+*/
+@WebServlet("/PostHandler")
+public class PostHandler extends HttpServlet {
+        private static final long serialVersionUID = 1L;
+       
+    /**
+* @see HttpServlet#HttpServlet()
+*/
+    public PostHandler() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+        /**
+         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                // TODO Auto-generated method stub
+                
+        }}
+
+       
+
+
